d7c1295ffa7d6505e4fffe6fc6767a836481c111
michaldobiezynski <michaldobiezynski@hotmail.com>
2020-10-01 09:21:29 +0100

    designing home screen part 1

--- screens/Home.js
+++ screens/Home.js
@@ -1,19 +1,49 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
 import { Card } from "react-native-paper";
 
 const Home = () => {
-  return (
-    <Card style={styles.mycard}>
-      <Text>Hello from home!</Text>
-    </Card>
-  );
+  const data = [
+    { id: 1, name: "Michal", position: "web dev" },
+    { id: 2, name: "Bogdan", position: "android dev" },
+    { id: 3, name: "Jesse", position: "iOs dev" },
+    { id: 4, name: "Walt", position: "Ml dev" },
+  ];
+
+  const renderList = data.map((item) => {
+    return (
+      <Card style={styles.mycard} key={item.id}>
+        <View style={styles.cardView}>
+          <Image
+            style={{ width: 60, height: 50, borderRadius: 30 }}
+            source={{
+              uri:
+                "https://images.unsplash.com/photo-1552915170-73c2330ae617?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=80",
+            }}
+          />
+          <View style={{ marginLeft: 10 }}>
+            <Text style={styles.text}>{item.name}</Text>
+            <Text style={styles.text}>{item.position}</Text>
+          </View>
+        </View>
+      </Card>
+    );
+  });
+  return <View>{renderList}</View>;
 };
 
 const styles = StyleSheet.create({
   mycard: {
     margin: 5,
-    padding: 5,
+    flexDirection: "row",
+  },
+  cardView: {
+    flexDirection: "row",
+    padding: 6,
+  },
+  text: {
+    fontSize: 20,
+    marginLeft: 10,
   },
 });
 
