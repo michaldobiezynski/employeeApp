77a779d29181d250a70c296544ed3b745ac718ef
michaldobiezynski <michaldobiezynski@hotmail.com>
2020-10-03 12:45:33 +0100

    designing profile screen part 2

--- screens/Profile.js
+++ screens/Profile.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
 
 import { LinearGradient } from "expo-linear-gradient";
-import { TextInput, Button } from "react-native-paper";
+import { MaterialIcons, Entypo } from "@expo/vector-icons";
+import { Title, Card, Button } from "react-native-paper";
 
 const Profile = () => {
   return (
@@ -25,13 +26,74 @@ const Profile = () => {
           }}
         />
       </View>
+      <View style={{ alignItems: "center", margin: 15 }}>
+        <Title>Michal</Title>
+        <Text style={{ fontSize: 15 }}>Web Develop</Text>
+      </View>
+      <Card style={styles.myCard}>
+        <View style={styles.cardContent}>
+          <MaterialIcons name="email" size={32} color="#006aff" />
+          <Text style={styles.myText}>abc@abc.com</Text>
+        </View>
+      </Card>
+      <Card style={styles.myCard}>
+        <View style={styles.cardContent}>
+          <Entypo name="phone" size={32} color="#006aff" />
+          <Text style={styles.myText}>112134123</Text>
+        </View>
+      </Card>
+      <Card style={styles.myCard}>
+        <View style={styles.cardContent}>
+          <MaterialIcons name="attach-money" size={32} color="#006aff" />
+          <Text style={styles.myText}>$100000</Text>
+        </View>
+      </Card>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-around",
+          padding: 10,
+        }}>
+        <Button
+          theme={theme}
+          icon="account-edit"
+          mode="contained"
+          onPress={() => console.log("Pressed")}>
+          Edit
+        </Button>
+        <Button
+          theme={theme}
+          icon="delete"
+          mode="contained"
+          onPress={() => console.log("Pressed")}>
+          Fire employee
+        </Button>
+      </View>
     </View>
   );
+};
+
+const theme = {
+  colors: {
+    primary: "#006aff",
+  },
 };
 
 const styles = StyleSheet.create({
   root: {
     flex: 1,
+  },
+  myCard: {
+    margin: 3,
+  },
+  cardContent: {
+    flexDirection: "row",
+    padding: 8,
+  },
+  myText: {
+    fontSize: 18,
+    marginTop: 3,
+    marginLeft: 5,
   },
 });
 
