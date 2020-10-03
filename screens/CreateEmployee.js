aef4d3ea319d1b9b98f5e4c86f1285c78ac899b9
michaldobiezynski <michaldobiezynski@hotmail.com>
2020-10-03 11:38:05 +0100

    adding modal (popup)

--- screens/CreateEmployee.js
+++ screens/CreateEmployee.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Modal } from "react-native";
 
-import { TextInput } from "react-native-paper";
+import { TextInput, Button } from "react-native-paper";
 
 const CreateEmployee = () => {
   const [name, setName] = useState("");
@@ -46,6 +46,51 @@ const CreateEmployee = () => {
         theme={theme}
         onChangeText={(text) => setSalary(text)}
       />
+      <Button
+        style={styles.inputStyle}
+        theme={theme}
+        icon="upload"
+        mode="contained"
+        onPress={() => setModal(true)}>
+        Upload Image
+      </Button>
+      <Button
+        style={styles.inputStyle}
+        theme={theme}
+        icon="content-save"
+        mode="contained"
+        onPress={() => setModal(true)}>
+        Save
+      </Button>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modal}
+        onRequestClose={() => {
+          setModal(false);
+        }}>
+        <View style={styles.modalView}>
+          <View style={styles.modalButtonView}>
+            <Button
+              theme={theme}
+              icon="camera"
+              mode="contained"
+              onPress={() => console.log("Pressed camera")}>
+              Camera
+            </Button>
+            <Button
+              theme={theme}
+              icon="image-area"
+              mode="contained"
+              onPress={() => console.log("Pressed gallery")}>
+              Gallery
+            </Button>
+          </View>
+          <Button theme={theme} onPress={() => setModal(false)}>
+            Cancel
+          </Button>
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -61,6 +106,17 @@ const styles = StyleSheet.create({
   },
   inputStyle: {
     margin: 5,
+  },
+  modalButtonView: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    padding: 10,
+  },
+  modalView: {
+    position: "absolute",
+    bottom: 2,
+    width: "100%",
+    backgroundColor: "white",
   },
 });
 
