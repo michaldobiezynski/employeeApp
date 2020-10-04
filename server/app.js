c5fc7cbc5c04d8d722565ac492d235c537225858
michaldobiezynski <michaldobiezynski@hotmail.com>
2020-10-04 12:09:58 +0100

    making schema and connecting to mongodb

--- server/app.js
+++ server/app.js
@@ -3,6 +3,22 @@ const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const data = require("../env.json");
+require("./Employee");
+
+const Employee = mongoose.model("employee");
+
+mongoose.connect(data.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("connected to mongo");
+  console.log(Employee);
+});
+mongoose.connection.on("error", (error) => {
+  console.log("error connecting to mongo: " + error);
+});
 
 app.get("/", (req, res) => {
   console.log(data.MONGODB_URL);
