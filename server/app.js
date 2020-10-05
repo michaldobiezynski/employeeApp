b7b4077df4ecaf6dc70ab1209df59a6aeb49f059
michaldobiezynski <michaldobiezynski@hotmail.com>
2020-10-05 09:13:32 +0100

    making post/update/delete  data route handlers

--- server/app.js
+++ server/app.js
@@ -2,27 +2,86 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const data = require("../env.json");
 require("./Employee");
+const data = require("../env.json");
+
+app.use(bodyParser.json());
+app.use(express.json());
 
 const Employee = mongoose.model("employee");
 
-mongoose.connect(data.MONGODB_URL, {
+const mongoUri = data.MONGODB_URL;
+
+mongoose.connect(mongoUri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 mongoose.connection.on("connected", () => {
-  console.log("connected to mongo");
-  console.log(Employee);
+  console.log("connected to mongo yeahhh");
 });
-mongoose.connection.on("error", (error) => {
-  console.log("error connecting to mongo: " + error);
+mongoose.connection.on("error", (err) => {
+  console.log("error", err);
 });
 
 app.get("/", (req, res) => {
-  console.log(data.MONGODB_URL);
-  res.send("welcome to node js");
+  Employee.find({})
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/send-data", (req, res) => {
+  console.log(req.body);
+  const employee = new Employee({
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    picture: req.body.picture,
+    salary: req.body.salary,
+    position: req.body.position,
+  });
+  employee
+    .save()
+    .then((data) => {
+      console.log(data);
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/delete", (req, res) => {
+  Employee.findByIdAndRemove(req.body.id)
+    .then((data) => {
+      console.log(data);
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/update", (req, res) => {
+  Employee.findByIdAndUpdate(req.body.id, {
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    picture: req.body.picture,
+    salary: req.body.salary,
+    position: req.body.position,
+  })
+    .then((data) => {
+      console.log(data);
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.listen(3000, () => {
